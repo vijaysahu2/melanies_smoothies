eaed355f4f962187b07b212f5111d473841c7739
vijaysahu2 <80475211+vijaysahu2@users.noreply.github.com>
2025-03-20 20:00:26 +0530

    Update streamlit_app.py
    added use_container_width

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ if ingredients_list:
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+st.text(smoothiefroot_response.json(), use_container_width = True)
     
     
 
