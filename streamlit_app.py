7bf30fdb82fec3ac41e84a366308d53c3b85e9e6
vijaysahu2 <80475211+vijaysahu2@users.noreply.github.com>
2025-03-20 20:03:51 +0530

    Update streamlit_app.py
    updated code

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df  = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+            
     
     # st.write(ingredients_string)
 
@@ -52,9 +55,6 @@ if ingredients_list:
 
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df  = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
-    
     
 
 
